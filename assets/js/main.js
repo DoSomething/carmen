0c5501ce1a9c60bfd931e4724f0ffb307d57eb05
Andrea Gaither <angaither@gmail.com>
2016-02-09 16:28:34 -0500

    Create a rought lat-long array and add that to the map.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,17 +10,8 @@ var ReactDom = require('react-dom');
 // @TODO: eventually clean this file up and set up as app bootstrap.
 
 var dosomething = ['40.741023', '-73.991770'];
-var map = L.map('map').setView(dosomething, 10);
+var map = L.map('map').setView(dosomething, 2);
 
-// Get all the markers from the sabbaticals json.
-var latslongs = [];
-for (var i = 0; i < data.length; i++) {
-  // This only deals with the first sabbatical for now.
-  var lat =  data[i].sabbaticals[0].location.latitude;
-  var long =  data[i].sabbaticals[0].location.longitude;
-  latslongs[i] = '[' + lat + ',' + long + ']';
-}
-console.log(latslongs);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -33,9 +24,18 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 // Leaflet is pulled in via NPM, and seems to have trouble finding image directory.
 L.Icon.Default.imagePath = config.leaflet.imagePath;
 
-var marker = L.marker(dosomething).addTo(map);
+// var marker = L.marker(dosomething).addTo(map);
 
+// Get all the markers from the sabbaticals json.
+var latslongs = [];
+for (var i = 0; i < data.length; i++) {
+  // This only deals with the first sabbatical for now.
+  var lat =  data[i].sabbaticals[0].location.latitude;
+  var long =  data[i].sabbaticals[0].location.longitude;
+  latslongs[i] = [lat, long];
+  L.marker(latslongs[i]).addTo(map);
 
+}
 
 // Testing use of React components:
 var Sabbatical = React.createClass({
